7005acea531082fe638bf254aafc4b3a2f004342
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2021-06-22 10:44:22 +0200

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name="nlse",
+    version="0.1",
+    description=(
+        "The simulation and the plotting codes for simulating pulse "
+        "propagation in systems governed by a Generalized Nonlinear "
+        "Schrödinger Equation (GNLSE)"),
+    author="Ivan Oreshnikov",
+    author_email="oreshnikov.ivan@gmail.com",
+    python_requires=">=3.5",
+    install_requires=[
+        "humanize==3.5.0",
+        "matplotlib==3.3.0",
+        "numpy==1.19.1",
+        "scipy==1.5.2"
+    ],
+    scripts=[]
+)
